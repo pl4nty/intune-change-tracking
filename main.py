5f6b29ee05d50788d4b673484d48f76b1b3e12bc
Tom Plant <tom@tplant.com.au>
2024-02-17 06:16:16 +0000

    feat: initial commit
    Signed-off-by: Tom Plant <tom@tplant.com.au>

---
+++ main.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import asyncio
+import json
+import os
+from pathlib import Path
+
+from azure.identity.aio import EnvironmentCredential
+from msgraph_beta import GraphServiceClient
+from kiota_abstractions.native_response_handler import NativeResponseHandler
+from kiota_http.middleware.options import ResponseHandlerOption
+
+from msgraph_beta.generated.device_management.configuration_settings.configuration_settings_request_builder import ConfigurationSettingsRequestBuilder
+
+client = GraphServiceClient(EnvironmentCredential(), ['https://graph.microsoft.com/.default'])
+
+async def main():
+    output = 'settings'
+
+    query_params = ConfigurationSettingsRequestBuilder.ConfigurationSettingsRequestBuilderGetQueryParameters(
+        top=10
+    )
+    request_config = ConfigurationSettingsRequestBuilder.ConfigurationSettingsRequestBuilderGetRequestConfiguration(
+        options=[ResponseHandlerOption(NativeResponseHandler())],
+        # query_parameters=query_params
+    )
+    settings = await client.device_management.configuration_settings.get(request_configuration=request_config)
+    
+    os.makedirs(output, exist_ok=True)
+    for setting in settings.json()['value']:
+        setting.pop('version')
+        path = Path(output, setting['id']).with_suffix('.json')
+        with open(path, 'w', encoding='utf-8') as f:
+            json.dump(setting, f, ensure_ascii=False, indent=4)
+
+asyncio.run(main())
