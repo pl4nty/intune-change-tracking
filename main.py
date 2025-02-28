61d23872a33b2eaf535fd6d5d65ef04c9cd7bd68
Tom Plant <tom@tplant.com.au>
2025-02-28 23:51:53 +1100

    feat: add Office Cloud Policy Service (#71)

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
+from msgraph_beta.generated.security.microsoft_graph_security_run_hunting_query.run_hunting_query_post_request_body import RunHuntingQueryPostRequestBody
+from msgraph_beta.generated.security.microsoft_graph_security_run_hunting_query.microsoft_graph_security_run_hunting_query_request_builder import MicrosoftGraphSecurityRunHuntingQueryRequestBuilder
+from msgraph_beta.generated.device_management.configuration_settings.configuration_settings_request_builder import ConfigurationSettingsRequestBuilder
+from kiota_abstractions.native_response_handler import NativeResponseHandler
+from kiota_http.middleware.options import ResponseHandlerOption
+from kiota_abstractions.base_request_configuration import RequestConfiguration
+import requests
+from msgraph_beta import GraphServiceClient
+from azure.core.credentials import AccessToken
+from azure.identity.aio import DefaultAzureCredential
+from pathlib import Path
+import time
+import subprocess
+import shutil
+import re
+import os
+import json
+import asyncio
+import aiohttp
 from dotenv import load_dotenv
 load_dotenv()
 
-import aiohttp
-import asyncio
-import json
-import os
-import re
-import shutil
-import subprocess
-import time
-from pathlib import Path
 
-from azure.identity.aio import DefaultAzureCredential
-from azure.core.credentials import AccessToken
-from msgraph_beta import GraphServiceClient
-import requests
-
-from kiota_abstractions.base_request_configuration import RequestConfiguration
-from kiota_http.middleware.options import ResponseHandlerOption
-from kiota_abstractions.native_response_handler import NativeResponseHandler
-
-from msgraph_beta.generated.device_management.configuration_settings.configuration_settings_request_builder import ConfigurationSettingsRequestBuilder
-from msgraph_beta.generated.security.microsoft_graph_security_run_hunting_query.microsoft_graph_security_run_hunting_query_request_builder import MicrosoftGraphSecurityRunHuntingQueryRequestBuilder
-from msgraph_beta.generated.security.microsoft_graph_security_run_hunting_query.run_hunting_query_post_request_body import RunHuntingQueryPostRequestBody
-
-# id_10699 -> id
 def cleanDCv1Ids(setting):
+    # id_10699 -> id
     id = '_'.join(setting.get('id').split('_')[:-1])
     setting['id'] = id
     for child in setting.get('childSettings', []):
@@ -37,11 +34,13 @@ def cleanDCv1Ids(setting):
         if metadata := column.get('metadata'):
             cleanDCv1Ids(metadata)
 
+
 async def main():
     # Setting status errors
     async with aiohttp.ClientSession() as session, session.get('https://intune.microsoft.com/signin/idpRedirect.js') as resp:
         versions = await resp.text()
-        versions = re.search(r'\"extensionsPageVersion\":({[^}]+})', versions).group(1)
+        versions = re.search(
+            r'\"extensionsPageVersion\":({[^}]+})', versions).group(1)
         versions = json.loads(versions)
 
         root = 'https://afd-v2.hosting.portal.azure.net'
@@ -50,8 +49,10 @@ async def main():
         # map setting error codes to descriptions
         async with session.get(f'{root_devicesettings}/Blades/DevicePoliciesStatus/SettingStatus.js') as resp:
             data = await resp.text()
-            data = re.search(r'SettingStatusErrorMap = ({[^}]+})', data).group(1)
-            data = json.loads(data, strict=False) # some strings have control characters
+            data = re.search(
+                r'SettingStatusErrorMap = ({[^}]+})', data).group(1)
+            # some strings have control characters
+            data = json.loads(data, strict=False)
             with open('SettingStatusErrors.json', 'w', encoding='utf-8') as f:
                 json.dump(data, f, ensure_ascii=False, indent=4)
 
@@ -73,7 +74,8 @@ async def main():
                         with open(path, 'w', encoding='utf-8') as f:
                             json.dump(setting, f, ensure_ascii=False, indent=4)
 
-    client = GraphServiceClient(DefaultAzureCredential(), ['https://graph.microsoft.com/.default'])
+    client = GraphServiceClient(DefaultAzureCredential(), [
+                                'https://graph.microsoft.com/.default'])
     request_config = RequestConfiguration(
         options=[ResponseHandlerOption(NativeResponseHandler())],
     )
@@ -125,7 +127,8 @@ async def main():
         ))
         if data.json().get('results') is not None:
             with open(f'Defender/{table}.json', 'w', encoding='utf-8') as f:
-                json.dump(data.json().get('results'), f, ensure_ascii=False, indent=4)
+                json.dump(data.json().get('results'), f,
+                          ensure_ascii=False, indent=4)
 
     # DCv2 configurationSettings eg Settings Catalog
     output = 'DCv2'
@@ -181,11 +184,11 @@ async def main():
     # Planned changes or new features in Microsoft Entra via ChangeManagementHub client
     client = GraphServiceClient(RefreshTokenCredential(
         '9d15ec9c-4104-48aa-9688-c907238f257b',
-        os.environ['AZURE_CHANGEMGMT_RT'],
+        'AZURE_CHANGEMGMT_RT',
         'c44b4083-3bb0-49c1-b47d-974e53cbdf3c',
         'brk-c44b4083-3bb0-49c1-b47d-974e53cbdf3c://entra.microsoft.com'
     ), ['https://graph.microsoft.com/.default'])
-    
+
     changes = []
     next = 'https://graph.microsoft.com/beta/identity/productChanges'
     while next is not None:
@@ -195,6 +198,36 @@ async def main():
         next = data.get('@odata.nextLink')
     with open('IdentityProductChanges.json', 'w', encoding='utf-8') as f:
         json.dump(changes, f, ensure_ascii=False, indent=4)
+
+    # Office Cloud Policy Service
+    token = RefreshTokenCredential(
+        '3cf6df92-2745-4f6f-bbcf-19b59bcdb62a',
+        'AZURE_OCPS_RT',
+    ).get_token('https://config.office.net//.default').token
+    headers = {'Authorization': f'Bearer {token}',
+               'User-Agent': 'github.com/pl4nty'}
+    for URL in [
+        # 'https://clients.config.office.net/releases/v1.0/FileList/channelsForProductIds?productId=O365ProPlusRetail&productId=O365ProPlusEEANoTeamsRetail',
+        # 'https://clients.config.office.net/onboarding/odata/v1.0/Agreementdata',
+        'https://clients.config.office.net/odbhealth/v1.0/synchealth/reports/versioncount',
+        'https://clients.config.office.net/releases/v1.0/FileList/languagesForProductIds?productId=O365ProPlusRetail',
+        'https://config.office.com/appConfig/v1.0/userflights',
+        # 'https://config.office.com/policyadmin/v1.0/policies',
+        'https://clients.config.office.net/settings/v1.0/SettingsCatalog/Settings',
+        'https://config.office.com/appConfig/v1.0/ServiceHealth',
+        # 'https://config.office.com/onboarding/odata/v1.0/FeatureProvisiondata',
+        'https://clients.config.office.net/releases/v1.0/OfficeReleases',
+        # POST https://clients.config.office.net/intents/odata/v1.0/ComponentGroupIntent {"componentGroupId": "a4e9e0f7-28cc-4304-98bc-fe2cb31121e0", "adminSelection": "off"} controls WebView2 autoinstall, or 675c828d-2e53-4ace-9697-75c5264b31c4 for Teams
+    ]:
+        data = requests.get(URL, headers=headers)
+        with open(f'OCPS/{URL.split('/')[-1].split('?')[0]}.json', 'w', encoding='utf-8') as f:
+            json.dump(data.json(), f, ensure_ascii=False, indent=4)
+
+    data = requests.get(
+        'https://clients.config.office.net/onboarding/odata/v1.0/FeatureData', headers=headers)
+    with open(f'OCPS/FeatureData.json', 'w', encoding='utf-8') as f:
+        json.dump(data.json()['value'], f, ensure_ascii=False, indent=4)
+
 
 class IbizaTokenCredential(object):
     def __init__(self, portalAuthorization, extensionName, resourceName):
@@ -207,33 +240,41 @@ class IbizaTokenCredential(object):
 
     def get_token(self, *scopes: str, claims=None, tenant_id=None, **kwargs):
         data = requests.post('https://intune.microsoft.com/api/DelegationToken', json=self._body,
-                             cookies={'portalId': 'f4a17c62-20c9-44b4-bde0-9206b1578bd2'}).json() # authHeader is null without portalId 
-        subprocess.run(['gh', 'secret', 'set', 'AZURE_INTUNEPORTAL_RT', '--body', data['portalAuthorization'], '--repo', os.environ['REPO']])
+                             # authHeader is null without portalId
+                             cookies={'portalId': 'f4a17c62-20c9-44b4-bde0-9206b1578bd2'}).json()
+        subprocess.run(['gh', 'secret', 'set', 'AZURE_INTUNEPORTAL_RT', '--body',
+                       data['portalAuthorization'], '--repo', os.environ['REPO']])
 
         token = data['value']['authHeader'].split()[1]
         return AccessToken(token, expires_on=data['value']['expiresAt'])
 
+
 class RefreshTokenCredential(object):
-    def __init__(self, client_id, refresh_token, brk_client_id = None, redirect_uri = None):
-        self._headers={}
-        self._body = {
-          'grant_type': 'refresh_token',
-          'client_id': client_id,
-          'refresh_token': refresh_token,
-          'scope': 'https://graph.microsoft.com//.default openid profile offline_access'
+    def __init__(self, client_id, token_envvar, brk_client_id=None, redirect_uri=None):
+        self._headers = {
+            'Origin': 'https://microsoft.com'
         }
-        
+        self._body = {
+            'grant_type': 'refresh_token',
+            'client_id': client_id,
+            'refresh_token': os.environ[token_envvar],
+        }
+        self._token_envvar = token_envvar
+
         if brk_client_id is not None:
-            self._headers['Origin'] = 'https://microsoft.com' # any origin
             self._body['brk_client_id'] = brk_client_id
             self._body['redirect_uri'] = redirect_uri
 
-    def get_token(self, *scopes: str, claims = None, tenant_id = None, **kwargs):
-        data = requests.post(f'https://login.microsoftonline.com/{os.environ["AZURE_TENANT_ID"]}/oauth2/v2.0/token', self._body, headers=self._headers).json()
-        
+    def get_token(self, *scopes: str, claims=None, tenant_id=None, **kwargs):
+        self._body['scope'] = scopes
+        data = requests.post(
+            f'https://login.microsoftonline.com/{os.environ["AZURE_TENANT_ID"]}/oauth2/v2.0/token', self._body, headers=self._headers).json()
+
         self._body['refresh_token'] = data['refresh_token']
-        subprocess.run(['gh', 'secret', 'set', 'AZURE_CHANGEMGMT_RT', '--body', data['refresh_token'], '--repo', os.environ['REPO']])
-        
+        subprocess.run(['gh', 'secret', 'set', self._token_envvar,
+                       '--body', data['refresh_token'], '--repo', os.environ['REPO']])
+
         return AccessToken(token=data['access_token'], expires_on=int(time.time()+data['expires_in']))
+
 
 asyncio.run(main())
